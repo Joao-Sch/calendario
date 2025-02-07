1f07b07ff8e75ee61923e810e037c0c1e19ba705
Joao-Sch <joao.schvengber@gmail.com>
2025-02-07 23:07:15 +0000

    Adicionando o arquivo page2.tsx

---
+++ src/app/page2.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MyCalendar } from "../components/myConponent/myCalendar/myCalendar";
+
+export default function Page2() {
+  return (
+    <>
+      <div style={{justifyContent: "center",}}>
+        <MyCalendar />
+      </div>
+    </>
+  );
+}
